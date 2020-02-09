04b99627651e51606160ee2c39bc1b8b8134b1b8
gdb <gdb@ubuntu.(none)>
2020-02-09 10:43:59 -0800

    Creating HelloWorld in Assembly

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -7,6 +7,19 @@ global _start
 section .text
 _start:
 
+;	INS DEST SOURCE
+	mov eax, 0x4
+	mov ebx, 0x1
+	mov ecx, message
+;	mov edx, 0xc not sure this works
+	mov edx, mlen
+	int 0x80
+	
+	; exit the program gracefully
+	mov eax, 0x1
+	mov ebx, 0x5
+	int 0x80
 
 section .data
 	message: db "Hello World!"
+	mlen equ $-message
